948a9c7c00cd279333f076482060a1f22f2dc013
eddieb <eddiewird@gmail.com>
2024-07-16 18:29:23 -0400

    delete files after use and fixed everything

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, send_file, jsonify, redirect
+from flask import Flask, request, render_template, send_file, jsonify, redirect, after_this_request
 import subprocess
 import os
 import shutil
@@ -6,8 +6,6 @@ import uuid
 import imageio_ffmpeg as ffmpeg
 import threading
 import time
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 DOWNLOAD_DIR = './downloads'
@@ -15,16 +13,37 @@ DOWNLOAD_DIR = './downloads'
 # Store session data in a dictionary
 sessions = {}
 
+# del session directory
+def cleanup_session(session_id):
+    session_download_dir = os.path.join(DOWNLOAD_DIR, session_id)
+    session_download_zip = os.path.join(DOWNLOAD_DIR, session_id, ".zip")
+    if os.path.exists(session_download_dir):
+        shutil.rmtree(session_download_dir)
+    
+    if os.path.exists(session_download_zip):
+        os.remove(session_download_zip)
+
+
+def remove_session(session_id):
+    if session_id in sessions:
+        del sessions[session_id]
+
+def remove_sessions():
+    global sessions
+    sessions = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/download', methods=['POST'])
 def download_song():
+    remove_sessions()
     song_url = request.form['song_url']
     session_id = str(uuid.uuid4())
     session_download_dir = os.path.join(DOWNLOAD_DIR, session_id)
-    
+    sessions[session_id] = {'status': 'started', 'progress': 0}
+
     try:
         # Create a unique download directory for this session
         os.makedirs(session_download_dir, exist_ok=True)
@@ -43,34 +62,10 @@ def download_song():
         # Check if the process was successful
         if result.returncode == 0:
             return redirect(f"/download_zip/{session_id}")
-            return jsonify({'success': True, 'session_id': session_id})
         else:
             return jsonify({'success': False, 'error_message': f"Error: {result.stderr}"})
     except Exception as e:
         return jsonify({'success': False, 'error_message': f"Unexpected error: {str(e)}"})
-
-
-@app.route('/get_id', methods=['GET'])
-def get_id():
-    session_id = request.args.get('session_id')
-    if session_id in sessions:
-        return jsonify({'success': True, 'session_id': session_id})
-    return jsonify({'success': False, 'error_message': 'Invalid session ID'})
-
-@app.route('/download_progress/<session_id>')
-def download_progress(session_id):
-    session_download_dir = os.path.join(DOWNLOAD_DIR, session_id)
-    if session_id not in sessions:
-        return jsonify({'success': False, 'error_message': 'Invalid session ID'})
-    try:
-        total_items = len(os.listdir(session_download_dir))
-        downloaded_items = len([name for name in os.listdir(session_download_dir) if os.path.isfile(os.path.join(session_download_dir, name))])
-        percentage = int((downloaded_items / total_items) * 100) if total_items > 0 else 0
-        sessions[session_id]['progress'] = percentage
-
-        return jsonify({'success': True, 'percentage': percentage, 'total_items': total_items})
-    except Exception as e:
-        return jsonify({'success': False, 'error_message': str(e)})
 
 @app.route('/download_zip/<session_id>')
 def download_zip(session_id):
@@ -80,7 +75,40 @@ def download_zip(session_id):
     session_download_dir = os.path.join(DOWNLOAD_DIR, session_id)
     zip_file = f"{session_download_dir}.zip"
     shutil.make_archive(session_download_dir, 'zip', session_download_dir)
+    
+    # Schedule cleanup after response is sent
+    @after_this_request
+    def remove_files(response):
+        cleanup_session(session_id)
+        return response
+    
     return send_file(zip_file, as_attachment=True)
+
+
+@app.route('/get_id', methods=['GET'])
+def get_id():
+    time.sleep(1)
+    session_key = list(sessions.keys())
+    key = session_key[0]
+    if len(session_key) != 1:
+        print("yo waht")
+        print(session_key)
+    if key in sessions:
+        return jsonify({'success': True, 'session_id': session_key, 'session_data': sessions})
+    return jsonify({'success': False, 'error_message': 'Invalid session ID'})
+
+@app.route('/download_progress/<session_id>')
+def download_progress(session_id):
+    if session_id not in sessions:
+        return jsonify({'success': False, 'error_message': 'Invalid session ID'})
+    session_download_dir = os.path.join(DOWNLOAD_DIR, session_id)
+    try:
+        total_items = len(os.listdir(session_download_dir))
+
+        return jsonify({'success': "True", 'total_items': total_items})
+    except Exception as e:
+        return jsonify({'success': "False", 'error_message': str(e)})
+
 
 if __name__ == "__main__":
     if not os.path.exists(DOWNLOAD_DIR):
--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,36 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const form = document.getElementById('downloadForm');
-//     const downloadButton = document.getElementById('downloadButton');
-//     const progressDiv = document.getElementById('progress');
-//     const progressText = document.querySelector('.progress-text');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('downloadForm');
+    const downloadButton = document.getElementById('downloadButton');
+    const progressDiv = document.getElementById('progress');
+    const progressText = document.querySelector('.progress-text');
 
-//     form.addEventListener('submit', async (event) => {
-//         event.preventDefault();
-//         downloadButton.disabled = true;
-//         progressDiv.style.display = 'flex';
-//         progressDiv.style.justifyContent = 'center';
+    form.addEventListener('submit', async (event) => {
+        downloadButton.disabled = true;
+        progressDiv.style.display = 'flex';
+        progressDiv.style.justifyContent = 'center';
 
-//         const response = await fetch('/download', {
-//             method: 'POST',
-//             body: formData
-//         });
+        const response = await fetch('/get_id', {
+            method: 'GET',
+        });
+        
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            const session_id = jsonResponse.session_id;
 
-//         if (response.ok) {
-//             const jsonResponse = await response.json();
-//             console.log(jsonResponse);
-//             const session_id = jsonResponse.session_id;
-//             console.log(`Session ID: ${session_id}`);
-
-//             // Polling for progress updates
-//             const intervalId = setInterval(async () => {
-//                 const progressResponse = await fetch(`/download_progress/${session_id}`);
-//                 if (progressResponse.ok) {
-//                     const progress = await progressResponse.json();
-//                     progressText.textContent = `${progress.total_items} items`;
-
-//                     if (progress.percentage === 100) {
-//                         clearInterval(intervalId);
-//                         window.location.href = `/download_zip/${session_id}`;
-//                     }
-//                 }
-//             }, 1000);
-//         } else {
-//             alert('Error downloading file.');
-//             downloadButton.disabled = false;
-//             progressDiv.style.display = 'none';
-//         }
-//     });
-// });
+            // Polling for progress updates
+            const intervalId = setInterval(async () => {
+                const progressResponse = await fetch(`/download_progress/${session_id}`);
+                if (progressResponse.ok) {
+                    const progress = await progressResponse.json();
+                    if (progress.total_items ) {
+                        progressText.textContent = `${progress.total_items} items`;
+                    }
+                }
+            }, 1000);
+        } else {
+            alert('Error downloading file.');
+            downloadButton.disabled = false;
+            progressDiv.style.display = 'none';
+        }
+    });
+});
